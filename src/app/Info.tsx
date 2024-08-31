087ba5d0f58bc990c1a7d88748e40a8232be182e
Aparna Gupta <aparnagupta813@gmail.com>
2024-08-31 23:28:01 +0530

    updates

--- src/app/Info.tsx
+++ src/app/Info.tsx
@@ -22,7 +22,7 @@ const Info: React.FC<InfoProps> = ({temperature, humidity, windSpeed, rainAccumu
    <p className=" text-3xl font-bold text-center mt-4">  {place} </p>
    </div>
   
-
+   
 
 
     </div>
@@ -33,14 +33,13 @@ const Info: React.FC<InfoProps> = ({temperature, humidity, windSpeed, rainAccumu
    <div>
    <div className='flex justify-between w-full text-xl'>
      <div className="flex h-20 mt-5">  <img  src=' https://cdn-icons-png.flaticon.com/512/1779/1779817.png ' alt='humidity-icon' className='w-9 h-9 '/> <p className='mt-1 ml-4 '> Humidity: <span className='text-2xl font-bold '>{ humidity ?  `${humidity}%` : "Data N/A"}</span> </p> </div>
-   <div className="flex h-20"> <img  src='https://www.fpl.com/content/dam/fplgp/us/en/icons/air-blue-icon.png' alt='windspeed-icon' className='w-12 h-12 mt-4 '/> <p className='mt-6 ml-4 '> Wind Speed :  <span className='text-2xl font-bold' >{windSpeed ? `${windSpeed}Km/h` : "Data N/A"}</span>  </p> </div>
+   <div className="flex h-20"> <img  src='https://www.fpl.com/content/dam/fplgp/us/en/icons/air-blue-icon.png' alt='windspeed-icon' className='w-12 h-12 mt-4 '/> <p className='mt-6 ml-4 '> Wind Speed :  <span className='text-2xl font-bold' >{windSpeed ? `${windSpeed} Km/h` : "Data N/A"}</span>  </p> </div>
    </div>
   
-     <div className='w-full flex justify-center mt-6'>
+     <div className='w-full flex justify-center  mt-3 md:mt-6'>
 
-      <div className='w-4/5 flex justify-between'>
+      <div className='w-11/12 lg:w-4/5 flex justify-between'>
       <div className="flex h-20"> <img  src='https://img.genial.ly/5ea5b2dcd331670fb93482a4/6e94be44-eb70-4b50-b909-0eea88d93033.png' alt='rain-intensity-icon' className='w-8 h-8 mt-4'/>  <p className='  mt-6 ml-2 '> Rain Intensity :  <span className=' font-bold '>{ rainIntensity ?  `${rainIntensity}mm/h` :  "Data N/A" }</span> </p> </div>
-      {/* <div className="flex h-20"> <img  src='https://png.pngtree.com/png-vector/20220610/ourmid/pngtree-wave-icon-on-white-background-png-image_4831129.png' alt='wind-direction-icon' className='w-8 h-8 mt-4'/> <p className='mt-2 ml-2'> {windDirection ? `Wind Direction: {windDirection}` :  "Data N/A" }  </p> </div></div> */}
 
       <div className="flex h-20"> <img  src='https://png.pngtree.com/png-vector/20220917/ourmid/pngtree-icon-depicting-severe-rainfall-torrential-downpour-heavy-precipitation-vector-png-image_35563458.png' alt='rain-accumulation-icon' className='w-8 h-8 mt-4'/>  <p className='mt-6 ml-2'>  Rain Accumulation: <span className=' font-bold'>{rainAccumulation ? `${rainAccumulation}mm` : "Data N/A" }</span> </p> </div>
       </div>
