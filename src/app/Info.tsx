4b07de0ff85915bf903ea7dfb8152b1e338aef5c
Aparna Gupta <aparnagupta813@gmail.com>
2024-09-01 02:16:18 +0530

    updates

--- src/app/Info.tsx
+++ src/app/Info.tsx
@@ -18,8 +18,8 @@ const Info: React.FC<InfoProps> = ({temperature, humidity, windSpeed, rainAccumu
     <div className='  '>
     <div className='flex justify-center'>
    <div>
-   {temperature ? <p className=" text-6xl font-bold text-center ">{temperature}&deg;C </p> : <p className=" text-6xl font-bold text-center"> Data N/A </p>}
-   <p className=" text-3xl font-bold text-center mt-4">  {place} </p>
+   {temperature ? <p className=" text-7xl font-bold text-center ">{temperature}&deg;C </p> : <p className=" text-6xl font-bold text-center"> Data N/A </p>}
+   <p className=" text-4xl font-bold text-center mt-4">  {place} </p>
    </div>
   
    
@@ -29,19 +29,19 @@ const Info: React.FC<InfoProps> = ({temperature, humidity, windSpeed, rainAccumu
    
   
 
-    <div className=' w-full'>
-   <div>
-   <div className='flex justify-between w-full text-xl'>
-     <div className="flex h-20 mt-5">  <img  src=' https://cdn-icons-png.flaticon.com/512/1779/1779817.png ' alt='humidity-icon' className='w-9 h-9 '/> <p className='mt-1 ml-4 '> Humidity: <span className='text-2xl font-bold '>{ humidity ?  `${humidity}%` : "Data N/A"}</span> </p> </div>
-   <div className="flex h-20"> <img  src='https://www.fpl.com/content/dam/fplgp/us/en/icons/air-blue-icon.png' alt='windspeed-icon' className='w-12 h-12 mt-4 '/> <p className='mt-6 ml-4 '> Wind Speed :  <span className='text-2xl font-bold' >{windSpeed ? `${windSpeed} Km/h` : "Data N/A"}</span>  </p> </div>
+    <div className=' w-full h-full pt-16 lg:pt-16 flex justify-center'>
+   <div className = " w-full md:w-11/12 lg:w-4/5">
+   <div className='flex justify-between w-full text-2xl'>
+     <div className="flex h-20 mt-5">  <img  src=' https://cdn-icons-png.flaticon.com/512/1779/1779817.png ' alt='humidity-icon' className='w-9 h-9 '/> <p className='mt-1 lg:ml-2 md:ml-4 '> Humidity: <span className=' text-2xl md:text-4xl font-bold '>{ humidity ?  `${humidity}%` : "Data N/A"}</span> </p> </div>
+   <div className="flex h-20"> <img  src='https://www.fpl.com/content/dam/fplgp/us/en/icons/air-blue-icon.png' alt='windspeed-icon' className='w-12 h-12 mt-4 '/> <p className='mt-6 lg:ml-2 md:ml-4 '> Wind Speed :  <span className='text-2xl md:text-4xl  font-bold' >{windSpeed ? `${windSpeed} Km/h` : "Data N/A"}</span>  </p> </div>
    </div>
   
-     <div className='w-full flex justify-center  mt-3 md:mt-6'>
+     <div className='w-full flex justify-center pt-20 lg:pt-6'>
 
-      <div className='w-11/12 lg:w-4/5 flex justify-between'>
-      <div className="flex h-20"> <img  src='https://img.genial.ly/5ea5b2dcd331670fb93482a4/6e94be44-eb70-4b50-b909-0eea88d93033.png' alt='rain-intensity-icon' className='w-8 h-8 mt-4'/>  <p className='  mt-6 ml-2 '> Rain Intensity :  <span className=' font-bold '>{ rainIntensity ?  `${rainIntensity}mm/h` :  "Data N/A" }</span> </p> </div>
+      <div className='w-11/12 lg:w-4/5 flex justify-between text-xl'>
+      <div className="flex h-20"> <img  src='https://img.genial.ly/5ea5b2dcd331670fb93482a4/6e94be44-eb70-4b50-b909-0eea88d93033.png' alt='rain-intensity-icon' className='w-8 h-8 mt-4'/>  <p className='  mt-6 ml-2 '> Rain Intensity :  <span className=' font-bold 2xl '>{ rainIntensity ?  `${rainIntensity}mm/h` :  "Data N/A" }</span> </p> </div>
 
-      <div className="flex h-20"> <img  src='https://png.pngtree.com/png-vector/20220917/ourmid/pngtree-icon-depicting-severe-rainfall-torrential-downpour-heavy-precipitation-vector-png-image_35563458.png' alt='rain-accumulation-icon' className='w-8 h-8 mt-4'/>  <p className='mt-6 ml-2'>  Rain Accumulation: <span className=' font-bold'>{rainAccumulation ? `${rainAccumulation}mm` : "Data N/A" }</span> </p> </div>
+      <div className="flex h-20"> <img  src='https://png.pngtree.com/png-vector/20220917/ourmid/pngtree-icon-depicting-severe-rainfall-torrential-downpour-heavy-precipitation-vector-png-image_35563458.png' alt='rain-accumulation-icon' className='w-8 h-8 mt-4'/>  <p className='mt-6 ml-2 2xl'>  Rain Accumulation: <span className=' font-bold'>{rainAccumulation ? `${rainAccumulation}mm` : "Data N/A" }</span> </p> </div>
       </div>
      
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -247,7 +247,7 @@ const handleStatus = (errStatus: any) => {
   <img src="https://i.gifer.com/origin/45/454ba38b4ce5b3fdc8796ed710769e69.gif"  className="h-screen w-screen absolute top-0 left-0 object-cover" />
   <div className=" w-screen h-screen absolute top-0 left-0 flex justify-center flex-col items-center" >
 
- <div className=" grey-transparent w-full md:w-4/5 lg:w-1/2 text-white p-8 rounded-md  shadow-2xl">  
+ <div className=" grey-transparent w-11/12 h-5/6 text-white p-3 lg:p-8  pt-56 md:pt-72 lg:pt-36 rounded-md  shadow-2xl">  
 
 { dataLoaded ?  <Info temperature = { localityWeatherData.temperature  } 
     humidity = { localityWeatherData.humidity  } 
@@ -263,30 +263,28 @@ const handleStatus = (errStatus: any) => {
    </div>
  </div>
 
-<div className= "w-screen" >  
-<div className=  {` w-screen flex justify-center  ${searchResultsShowing ? "h-48 md:h-52 lg:h-24 bg-white absolute top-0 left-0 py-3 px-2 lg:px-8 " : " bg-white lg:flex-col lg:items-center h-screen mt-20 lg:mt-0" }`}      > 
+<div className= {`${searchResultsShowing ? "flex justify-center" : "w-screen"}`} >  
+<div className=  {`  flex justify-center  ${searchResultsShowing ? "h-48 md:h-56 lg:h-24 pt-6 md:pt-12 lg:pt-2 bg-transparent absolute top-16  py-3 px-2 lg:px-8 w-11/12 border-b border-spacing-2" : " bg-white w-screen lg:flex-col lg:items-center h-screen mt-20 lg:mt-0" }`}      > 
   <div  className= {`${searchResultsShowing ? "  w-3/5 lg:w-full lg:flex lg:justify-start"  : "w-5/6 lg:w-auto"}`} >
  
 
-  <div onClick={() => {setSearchResultsShowing(false)}}  className={`font-bold  flex  ${searchResultsShowing ? "text-2xl md:text-4xl lg:w-1/5 lg:text-3xl mt-3 justify-center lg:justify-start " : "text-2xl lg:text-8xl w-full  justify-center" }`}> <p className="text-blue-600"> L </p>  <p className="text-red-500"> i </p> <p className="text-yellow-500"> v </p> <p className="text-blue-600"> e </p> <p className={`text-green-600 ml-1.5   ${searchResultsShowing ? "md:ml-1.5" : " md:ml-4"}`}> W </p> <p className="text-red-500"> e </p> <p className="text-yellow-500"> a </p> <p className="text-blue-600"> t </p> <p className="text-red-500"> h </p > <p className="text-green-600"> e </p> <p className="text-yellow-500"> r </p> </div>
+  <div onClick={() => {setSearchResultsShowing(false)}}  className={`font-bold  flex  ${searchResultsShowing ? " text-2xl md:text-4xl w-4/5 md:w-2/3 lg:w-1/5 lg:text-3xl mt-3 justify-center lg:justify-start bg-white rounded-3xl lg:px-5 lg:pt-2.5" : "text-2xl lg:text-8xl   justify-center" }`}> <p className="text-blue-600"> L </p>  <p className="text-red-500"> i </p> <p className="text-yellow-500"> v </p> <p className="text-blue-600"> e </p> <p className={`text-green-600 ml-1.5   ${searchResultsShowing ? "md:ml-1.5" : " md:ml-4"}`}> W </p> <p className="text-red-500"> e </p> <p className="text-yellow-500"> a </p> <p className="text-blue-600"> t </p> <p className="text-red-500"> h </p > <p className="text-green-600"> e </p> <p className="text-yellow-500"> r </p> </div>
 
-  <div className={`rounded-3xl  ${searchingByCoordinates ? "border-0"  :"border border-zinc-300 hover:shadow-md"  }   ${searchResultsShowing ? "w-full lg:ml-8  my-3 lg:my-0 ml-1 "  : "w-full  mt-6" }`}   >
-  <input className=   {`bg-white w-full rounded-3xl px-3 pt-1.5 lg:py-4 outline-0  hover:border-0 ${searchingByCoordinates ? "hidden" : "block" }  `}  type="text" value={userInput} onChange={handleChange} name = "userInputPlace" id = "userInput"  placeholder="Type Locality e.g. Brookefields"/>
+  <div className={`rounded-3xl  ${searchingByCoordinates ? "border-0"  :"border border-zinc-300 hover:shadow-md"  }   ${searchResultsShowing ? " w-full lg:ml-8  my-3 lg:my-0 ml-1 "  : "w-full  mt-6" }`}   >
+  <input className=   { `  w-full rounded-3xl px-3 pt-1 lg:pt-5 outline-0  hover:border-0 ${searchingByCoordinates ? "hidden" : "block" } ${searchResultsShowing ? "bg-transparent text-white font-bold" : "bg-white"} `}  type="text" value={userInput} onChange={handleChange} name = "userInputPlace" id = "userInput"  placeholder="Type Locality e.g. Brookefields"/>
   <div className=  {`justify-between ${searchingByCoordinates ? "flex" : "hidden" } `} > 
   <input className= {` font-bold rounded-3xl p-2.5 outline-0 hover:shadow-md border border-zinc-300 ${searchResultsShowing ? " w-28 md:w-52 lg:w-5/12 mt-3" : "w-5/12"} `}  placeholder="Latitude"  type="text" name= "latitude" value={latitude} onChange={(e) => setLatitude(e.target.value)}/>
   <input className= {`font-bold rounded-3xl p-2.5 outline-0 hover:shadow-md border border-zinc-300 ${searchResultsShowing ? " w-28  md:w-52 lg:w-5/12 mt-3" : "w-5/12"}`}  placeholder="Longitude"  type="text" name= "longitude" value={longitude} onChange={(e) => setLongitude(e.target.value)}/>
     
       </div>
-  {/* <div className="p-2 overflow-y-scroll max-h-64  rounded-3xl bg-white"> { suggestions &&  suggestions.map((item: string, i ) => <div key={i} className="hover:bg-slate-400" onClick={ () =>  {fetchWeatherfromUserInput(item)
-  
-}} > {item} </div>)}   */}
  
  
-<div className= {`${showSuggestions? "block" : "hidden" }`}>
-{suggestions &&  <div className= "p-2 overflow-y-scroll max-h-64  rounded-3xl bg-white flex justify-between">
-   <div>   {  suggestions.map((item: string, i ) => <div key={i} className="hover:bg-slate-400" onClick={ () =>  {fetchWeatherfromUserInput(item)
+ 
+<div className= {`${showSuggestions? "block w-full" : "hidden" }`}>
+{suggestions &&  <div className= "p-2 overflow-y-scroll max-h-64  rounded-3xl bg-white flex  justify-between w-full">
+   <div>   {  suggestions.map((item: string, i ) => <div key={i} className="hover:bg-slate-400 flex justify-center w-full" onClick={ () =>  {fetchWeatherfromUserInput(item)
   
-}} > {item} </div>)}  
+}} > <div className="w-full hover:bg-slate-400">  {item} </div> </div>)}  
  
   </div>
  <img  className= "w-8 h-8 mr-1 hover:bg-slate-200 rounded-full p-2"  onClick={() => {setShowSuggestions(false)}} src="https://cdn-icons-png.flaticon.com/512/748/748122.png"/>
@@ -306,7 +304,7 @@ const handleStatus = (errStatus: any) => {
 
   <div className=  {` lg:max-h-12 flex w-full text-sm lg:text-base  ${searchResultsShowing?  "mt-6 md:mt-3 lg:justify-end" : "mt-8 justify-center" }`} > 
     <button className={`bg-zinc-100 hover:border hover:border-zinc-300 rounded-md py-2 px-3 lg:px-6 text-sm text-slate-800 hover:shadow-sm mr-4 ${searchResultsShowing ? `${searchingByCoordinates ? "block"  : "hidden"}`: "" }`} onClick= {handleSearchbyPlaceNameBtn}  > Search by Place Name </button> 
-    <button className= {`${searchingByCoordinates?  "hidden" : "bg-zinc-100 hover:border hover:border-zinc-300 rounded-md py-2 lg:mt-3 px-3 lg:px-6 md:text-sm text-slate-800 hover:shadow-sm md:w-auto text-xs" }`}  onClick={ShowCheckWeatherBtn}> Search by Coordinates </button>
+    <button className= {`${searchingByCoordinates?  "hidden" : "bg-zinc-100 hover:border hover:border-zinc-300 rounded-md py-2  px-3 lg:px-6 md:text-sm text-slate-800 hover:shadow-sm md:w-auto text-xs" }`}  onClick={ShowCheckWeatherBtn}> Search by Coordinates </button>
     <button className= {`${searchingByCoordinates ? "bg-zinc-100 hover:border hover:border-zinc-300 rounded-md py-2  px-3 lg:px-6 md:text-sm text-slate-800 hover:shadow-sm md:w-auto text-xs" : "hidden"  }`}  onClick={handleSearchbyCoordinatesBtn}> Check Weather </button> 
   
     
